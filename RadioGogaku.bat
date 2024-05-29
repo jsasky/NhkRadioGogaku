ec199e1bc47ac2b2ca30ec7150dbdd4ed9930852
Saito Junya (uidg5622) <junya.saito@continental-corporation.com>
2024-05-29 11:08:18 +0900

    Modify path

--- RadioGogaku.bat
+++ RadioGogaku.bat
@@ -1,10 +1,5 @@
 chcp 65001
 
-C:\LegacyApp\Python36\python.exe RadioGogaku.py
-
-@REM @REM @REM https://cgi2.nhk.or.jp/gogaku/st/xml/english/kaiwa/listdataflv.xml
-@REM SET SERVER=https://nhks-vh.akamaihd.net/i/gogaku-stream/mp4/22-er-4235-525.mp4/master.m3u8
-@REM SET FILENAME=ラジオ英会話 2022年11月11日放送分
-@REM ffmpeg -http_seekable 0 -i %SERVER% -id3v2_version 3 -metadata artist="NHK" -metadata title="%FILENAME%" -metadata album="ラジオ英会話[2022年度]" -metadata date="2022" -ar 44100 -ab 64k -c:a mp3 -c:a mp3 "%FILENAME%.mp3"
+D:\Tools\Python\python.exe RadioGogaku.py
 
 pause
--- RadioGogakuJson.bat
+++ RadioGogakuJson.bat
@@ -1,5 +1,5 @@
 chcp 65001
 
-C:\LegacyApp\Python36\python.exe RadioGogakuJson.py
+D:\Tools\Python\python.exe RadioGogakuJson.py
 
 pause
